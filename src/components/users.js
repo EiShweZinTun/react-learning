0b22790b6e558b5e3c15ee2a0cbea00bbe8cf675
EiShweZin Tun <eitun@easysheets.net>
2022-06-01 12:31:15 +0630

    Add: Props,Components,usesState

---
+++ src/components/users.js
@@ -0,0 +1,34 @@
+import React ,{ useEffect, useState}from 'react'
+import User from '../components/user';
+
+const Users = () => {
+  
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() =>{
+    getUsers();
+  }, []);
+
+  console.log(userData);
+  const getUsers = async () => {
+     setLoading(true);
+     await fetch('https://randomuser.me/api/?results=100&nat=us')
+      .then((response) => response.json())
+      .then ((data) => setUserData(data.results));
+    setLoading(false);       
+  };
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        userData.map((data, dataIndex) => (
+        <User key={dataIndex} user={data}/>
+        ))
+       )}
+      </div>
+  );
+};
+
+export default Users;
